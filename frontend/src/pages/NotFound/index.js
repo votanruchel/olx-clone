c6259828e124a0168c2dc0124a7db1cd84c6f6d5
Votan Ruchel <votanruchel@hotmail.com>
2020-03-05 02:30:32 -0300

    Add Router NotFound page

---
+++ frontend/src/pages/NotFound/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+const Page = () => {
+    return (
+        <div>
+            <h3>Pagina nao encontrada</h3>
+            <Link to="/">Voltar para a Home</Link>
+        </div>
+
+    )
+}
+export default Page;

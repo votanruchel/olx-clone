b4a52fb091252a54224f339c27337ac31a324f9f
Votan Ruchel <votanruchel@MacBook-Air-de-Votan.local>
2020-04-12 21:45:02 -0300

    cats

--- frontend/src/pages/Ads/index.js
+++ frontend/src/pages/Ads/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import {useLocation, useHistory} from 'react-router-dom';
 import { PageArea } from './styled';
 import useApi from '../../helpers/OlxAPI';
 
@@ -9,10 +9,35 @@ import { PageContainer } from '../../components/MainComponents';
 import AdItem from '../../components/partials/AdItem';
 const Page = () => {
     const api = useApi();
+    const history = useHistory();
+    const useQueryString = () =>{
+        return new URLSearchParams(useLocation().search);
+    }
+    const query = useQueryString();
 
     const [stateList, setStateList] = useState([])
+    const [q, setQ] = useState( query.get('q') != null ? query.get('q') : '' );
+    const [cat, setCat] = useState(query.get('cat') != null ? query.get('cat') : '')
+    const [state, setState] = useState(query.get('state') != null ? query.get('state') : '')
+
     const [categories, setCategories] = useState([])
     const [adList, setAdList] = useState([])
+
+    useEffect(()=>{
+        let queryString = [];
+        if(q){
+            queryString.push(`q=${q}`);
+        }
+        if(cat){
+            queryString.push(`cat=${cat}`);
+        }
+        if(state){
+            queryString.push(`state=${state}`);
+        }
+        history.replace({
+            search:`?${queryString.join('&')}`
+        })
+    }, [q,cat,state])
 
     useEffect(()=>{
         const getStates = async () =>{
@@ -46,10 +71,20 @@ const Page = () => {
                 <PageArea>
                     <div className="leftSide">
                         <form method="GET">
-                            <input type='text' name="q" />
+                            <input 
+                            type='text' 
+                            name="q" 
+                            placeholder="O que voce procura ?" 
+                            value={q}
+                            onChange={e=>setQ(e.target.value)}
+                            />
                             <div className="filterName">
                                 Estado:
-                                <select name="state">
+                                <select 
+                                name="state" 
+                                value={state}
+                                onChange={e=>setState(e.target.value)}
+                                >
                                     <option></option>
                                     {stateList.map((i,k)=>
                                     <option value={i.name} key={k}>{i.name}</option>)}
@@ -59,7 +94,11 @@ const Page = () => {
                                 Categoria:
                                 <ul>
                                     {categories.map((i,k)=>
-                                    <li key={k} className="categoryItem">
+                                    <li 
+                                    key={k} 
+                                    className={cat==i.slug?'categoryItem active':'categoryItem'}
+                                    onClick={e=>setCat(i.slug)}
+                                    >
                                         <img src={i.img} alt=""/>
                                         <span>{i.name}</span>
                                     </li>
